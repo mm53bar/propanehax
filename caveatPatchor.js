57e3bc2b34c2e2d9315bfd5d25b829c1b2f85307
Aman Gupta <aman@tmm1.net>
2011-06-16 17:33:58 -0700

    more

--- caveatPatchor.js
+++ caveatPatchor.js
@@ -458,13 +458,31 @@ if (true) {
         if (body.innerText.match(/^Build #(\d+) \([0-9a-zA-Z]+\) of (github-)?([-_0-9a-zA-Z]+)/)) {
           var failed_p = body.innerText.match(/failed/);
           var success_p = body.innerText.match(/success/);
+          var color = failed_p ? '#ff0000' : '#00941f';
           if (failed_p || success_p)
             message.bodyCell.setStyle({
-              color: failed_p ? '#ff0000' : '#00941f',
+              color: color,
               fontWeight: 'bold'
             })
 
-          body.replace(body.outerHTML.replace(/#(\d+) \(([0-9a-zA-Z]+)\) of (?:github-)?([-_0-9a-zA-Z]+)/, '<a target="_blank" href="http://ci2.rs.github.com:8080/job/github-$3/$1/console">#$1</a> ($2) of github-$3'));
+          var sha = body.innerText.match(/\(([0-9a-z]+)\)/i)[1]
+          var build = body.outerHTML.replace(/#(\d+) \(([0-9a-zA-Z]+)\) of (?:github-)?([-_0-9a-zA-Z]+)/, '<a target="_blank" href="http://ci2.rs.github.com:8080/job/github-$3/$1/console">#$1</a> ($2) of github-$3')
+          body.replace(build)
+          build = build.replace(/^.*?<a/,'<a').replace(/<\/a>.*/, '</a>')
+
+          var msgIndex = this.chat.transcript.messages.indexOf(message);
+          if (msgIndex > -1) {
+            for (var i=msgIndex; i > 0 && i > msgIndex - 5; i--) {
+              var otherMsg = this.chat.transcript.messages[i]
+              if (otherMsg.element.innerHTML.match("/commit/" + sha)) {
+                build = build.replace(/<\/a>.*$/, '</a>').replace('Build ','');
+                otherMsg.bodyElement().insert({bottom: " ["+build+"]"})
+                otherMsg.bodyCell.setStyle({color:color,fontWeight:'bold'})
+                message.element.remove()
+                break
+              }
+            }
+          }
         }
       }
     },
